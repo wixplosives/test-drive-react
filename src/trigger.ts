c77de94a004beb30d2d9e33616ab5e6ff56b6396
Jiri Tobisek <jiri@wix.com>
2017-06-13 12:15:10 +0300

    Takoing advantage of test-drive/helpers

--- src/trigger.ts
+++ src/trigger.ts
@@ -1,28 +1,5 @@
 import triggerChange = require('react-trigger-change');
-
-export interface GenericInputElement {
-    focus(): void;
-    value: string;
-}
-
-function getGlobalsOf(element: Element): any {
-    if(element.ownerDocument && element.ownerDocument.defaultView) {
-        return element.ownerDocument.defaultView;
-    } else {
-        return window;
-    }
-}
-
-function isInputElement(element: Element): element is Element & GenericInputElement  {
-    const globalScope = getGlobalsOf(element);
-    const HTMLInputElement = globalScope['HTMLInputElement'];
-    const HTMLTextAreaElement = globalScope['HTMLTextAreaElement'];
-    const HTMLSelectElement = globalScope['HTMLSelectElement'];
-    return element instanceof  HTMLInputElement ||
-        element instanceof HTMLTextAreaElement ||
-        element instanceof HTMLSelectElement;
-}
-
+import {isInputElement} from "test-drive/helpers";
 
 export function change(target: Element | null, newValue: string): void {
     if(target) {
