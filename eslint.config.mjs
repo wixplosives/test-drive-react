7b7a76724c7114b47e02602100a6e8b1cab08450
Avi Vahl <avi.vahl@wix.com>
2024-08-15 16:46:14 +0300

    infra: re-enable lint for js/mjs/cjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,30 +4,37 @@ import pluginNoOnlyTests from 'eslint-plugin-no-only-tests';
 import pluginReact from 'eslint-plugin-react';
 import pluginTypescript from 'typescript-eslint';
 
+for (const config of pluginTypescript.configs.recommendedTypeChecked) {
+  config.files = ['**/*.{ts,tsx,mts,cts}']; // ensure config only targets TypeScript files
+}
+
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  { ignores: ['dist/', 'cjs/', 'esm/', '**/*.{js,mjs,cjs}'] },
+  { ignores: ['dist/', 'cjs/', 'esm/'] },
   pluginJs.configs.recommended,
-
-  ...pluginTypescript.configs.recommendedTypeChecked,
-  { languageOptions: { parserOptions: { projectService: true } } },
-
   pluginReact.configs.flat.recommended,
   // pluginReact.configs.flat["jsx-runtime"],
   { settings: { react: { version: 'detect' } } },
-
   { plugins: { 'no-only-tests': pluginNoOnlyTests } },
-  configPrettier,
-
   {
     rules: {
       'no-console': 'error',
+      'no-only-tests/no-only-tests': 'error',
+      'no-undef': 'off',
+      'no-unused-vars': ['error', { argsIgnorePattern: '^_', varsIgnorePattern: '^_' }],
       'react/no-deprecated': 'off',
       'react/prop-types': 'off',
-      'no-only-tests/no-only-tests': 'error',
+    },
+  },
+  ...pluginTypescript.configs.recommendedTypeChecked,
+  { languageOptions: { parserOptions: { projectService: true } } },
+  {
+    files: ['**/*.{ts,tsx,mts,cts}'],
+    rules: {
       '@typescript-eslint/no-empty-object-type': 'off',
-      '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_', varsIgnorePattern: '^_' }], // allow unused _ variables
+      '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_', varsIgnorePattern: '^_' }],
       '@typescript-eslint/unbound-method': 'off',
     },
   },
+  configPrettier,
 ];
