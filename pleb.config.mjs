e68ccfa5b71b484b0a1f2aa155a216b17a162808
Avi Vahl <avi@vahl.co.il>
2024-12-15 23:30:56 +0200

    chore(deps-dev): upgrade to latest stable - ignore react@19 for now - regenerate lock file

--- pleb.config.mjs
+++ pleb.config.mjs
@@ -2,5 +2,10 @@ export default {
   pinnedPackages: [
     { name: '@types/chai', reason: 'esm-only' },
     { name: '@types/sinon-chai', reason: 'esm-only' },
+    
+    { name: 'react', reason: 'v19 requires further changes' },
+    { name: '@types/react', reason: 'v19 requires further changes' },
+    { name: 'react-dom', reason: 'v19 requires further changes' },
+    { name: '@types/react-dom', reason: 'v19 requires further changes' },
   ],
 };
